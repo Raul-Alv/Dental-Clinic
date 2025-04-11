10a2656ebeae081b8cfc76c6476a20d0684a1e92
Raúl Álvarez <roloa@192.168.1.155>
2025-04-11 20:17:23 +0200

    update(modelo): Actualización del modelo a 0002
    Se actualiza el modelo de datos a 0002, en el que se añaden más campos para mas detalles.
    Se reorganizan los campos para que tengan un orden más lógico y consistente.

--- clinicaDental/clinica/models.py
+++ clinicaDental/clinica/models.py
@@ -5,22 +5,26 @@ class Paciente(models.Model):
     activo = models.BooleanField(default=True)
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
-    genero = models.CharField(max_length=10, choices=[('M', 'Masculino'), ('F', 'Femenino')])
+    genero = models.CharField(max_length=10, choices=[('M', 'Masculino'), ('F', 'Femenino'), ('O', 'Otro')], blank=False, default='O')
+    telefono = models.CharField(max_length=15, blank=True, null=True)
     fecha_nacimiento = models.DateField()
-    telefono = models.CharField(max_length=15)
+    direccion = models.CharField(max_length=255, blank=True, null=True)
+    estado_civil = models.CharField(max_length=10, choices=[('S', 'Soltero'), ('C', 'Casado'), ('D', 'Divorciado')], default='S')
+    #contacto = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
     
 class Practicante(models.Model):
     activo = models.BooleanField(default=True)
-    tratamiento = models.CharField(max_length=10, choices=[('Dr.', 'Doctor'), ('Dra.', 'Doctora')])
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
-    cualificacion = models.CharField(max_length=100)
+    genero = models.CharField(max_length=10, choices=[('M', 'Masculino'), ('F', 'Femenino'), ('O', 'Otro')], blank=False, default='O')
+    telefono = models.CharField(max_length=15, blank=True, null=True)
+    cualificacion = models.CharField(max_length=100) #Buscar como hacerlo más detallado
 
     def __str__(self):
-        return f"{self.tratamiento} {self.nombre} {self.apellido}"
+        return f"{"Dr." if {self.genero == "Masculino"} else "Dra."} {self.nombre} {self.apellido}"
 
 class Diente(models.Model):
     codigo = models.CharField(max_length=7, unique=True) #Codigo basado en ADA SNOMED 
@@ -30,14 +34,26 @@ class Diente(models.Model):
     def __str__(self):
         return f"{self.display}"
     
+class StatusProcedimiento(models.TextChoices):
+    PREPARACION = 'preparation', 'Preparación'
+    EN_PROGRESO = 'in-progress', 'En Progreso'
+    NO_REALIZADO = 'not-done', 'No Realizado'
+    EN_ESPERA = 'on-hold', 'En Espera'
+    PARADO = 'stopped', 'Parado'
+    COMPLETADO = 'completed', 'Completado'
+    CON_ERRORES = 'entered-in-error', 'Con Errores'
+    DESCONOCIDO = 'unknown', 'Desconocido'
+    
 class Procedimiento(models.Model):
     codigo = models.CharField(max_length=10, unique=True) #Codigo basado en SNOMED CT
-    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
-    practicante = models.ForeignKey(Practicante, on_delete=models.CASCADE)
-    diente = models.ForeignKey(Diente, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=StatusProcedimiento.choices, default=StatusProcedimiento.PREPARACION)
+    paciente = models.ForeignKey(Paciente, on_delete=models.PROTECT)
+    practicante = models.ForeignKey(Practicante, on_delete=models.PROTECT)
+    diente = models.ForeignKey(Diente, on_delete=models.PROTECT)
     descripcion = models.CharField(max_length=100)
     realizado_el = models.DateField()
     
 
     def __str__(self):
         return f"{self.paciente.nombre} - {self.diente.display}, {self.realizado_el}"
+    
