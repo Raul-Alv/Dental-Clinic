c8cfc48d63ec2b13a52a0f491b3e956ca404e6de
Raúl Álvarez <roloa@192.168.1.155>
2025-04-23 21:22:07 +0200

    feat(CRUD): Añadido una visión individual de los procedimientos
    Se añade una forma de , desde la lista, poder acceder a los datos de un procedimiento
    Para el futuro se puede cambiar los datos enseñados en ambas.

--- clinicaDental/clinica/urls.py
+++ clinicaDental/clinica/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", views.index, name='index'),
     path("procedimiento/", views.procedimiento_list, name='procedimiento_list'),
     #path("procedimiento/crear/", views.crearProcedimiento, name='crear_procedimiento'),
-    #path("procedimiento/<int:id>/", views.getProcedimiento, name='get_procedimiento'),
+    path("procedimiento/<int:id>/", views.getProcedimiento, name='get_procedimiento'),
 ]
